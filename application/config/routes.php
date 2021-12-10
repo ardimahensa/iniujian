5e5e3175018b6b89d99bdc0407a7dade3e6967ba
ardimahensa <ardimahensa11@gmail.com>
2021-12-10 20:30:26 +0700

    edit routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,10 +50,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'user';
-$route['404_override'] = 'user/notfound';
-$route['translate_uri_dashes'] = FALSE;
 $route['(:num)'] = '/user/ujian/$1';
 $route['selesai/(:num)'] = 'user/ujian_selesai/$1';
 $route['setting'] = 'user/setting';
 $route['logout'] = 'Login/logout';
+$route['404_override'] = 'user/notfound';
+$route['translate_uri_dashes'] = FALSE;
 
